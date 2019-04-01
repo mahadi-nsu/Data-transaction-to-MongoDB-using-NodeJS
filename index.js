c5a9d6a58290f73fd7fc0f70882b5b543cf3cb92
mahadi <mahadi.nsucse@gmail.com>
2019-04-01 13:01:39 +0600

    working with more complex query

--- index.js
+++ index.js
@@ -46,7 +46,15 @@ async function getCourses() {
             //passing filter here
             author: 'Mahadi',
             isPublished: true
-        });
+        })
+        .limit(10)
+        .sort({
+            name: 1
+        })
+        .select({
+            name: 1,
+            tags: 1
+        })
     console.log(courses);
 }
 
