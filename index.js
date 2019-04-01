dfb3b67425b47fa91e6903e30824ed40328de885
mahadi <mahadi.nsucse@gmail.com>
2019-04-01 12:16:19 +0600

    Data Insert to Mongo

---
+++ index.js
@@ -0,0 +1,48 @@
+//import
+const mongoose = require('mongoose');
+
+
+// Connection SetUP
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log("Connected to mongoDB"))
+    .catch((err) => console.error("Not Connected", err));
+
+
+// Schema
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+// Models
+//here course is a class
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    //Example course
+    //Should come from frontend 
+    //for example here i am giving the values
+    const course = new Course({
+        name: 'Live Training on Node',
+        author: 'Mahadi',
+        tags: ['Node', 'Express', 'Backend'],
+        isPublished: true
+    });
+
+    //Save to the Database
+    //This is a asynchrous work
+
+    const result = await course.save();
+    console.log(result);
+}
+
+
+
+
+//Calling the function 
+createCourse();
+// getCourses();
+
